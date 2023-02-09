db4d587f2242daee3f31df10a8d6a6c29b3bc462
patriciogalindo <patriciogalindo@gmail.com>
2023-02-08 23:32:07 -0500

    render

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(port, () => {
-    console.log(`listening in port ${port}`)
+app.listen((process.env.PORT || port), () => {
+    console.log(`listening in port ${(process.env.PORT || port)}`)
 })
 
 
